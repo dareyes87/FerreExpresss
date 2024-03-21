d171ea3d69c7d2a99b13395de4f32cec85175d02
DANIEL HAROLDO REYES GARCIA <dreyesg11@miumg.edu.gt>
2024-03-20 23:03:15 -0600

    Se incorporan las funciones de categoria y popular a la pantalla home

--- app/src/main/java/com/example/ferreexpress/Activity/Home.kt
+++ app/src/main/java/com/example/ferreexpress/Activity/Home.kt
@@ -2,16 +2,26 @@ package com.example.ferreexpress.Activity
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.GridLayout
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import com.example.ferreexpress.Adapter.SliderAdapter
+import com.example.ferreexpress.Adapter.CategoryAdapter
+import com.example.ferreexpress.Adapter.PopularAdapter
+import com.example.ferreexpress.Domain.CategoryDomain
 import com.example.ferreexpress.Domain.SliderItems
+import com.example.ferreexpress.Domain.itemsDomain
 import com.example.ferreexpress.R
 import com.example.ferreexpress.databinding.ActivityHomeBinding
 import com.google.firebase.database.DataSnapshot
@@ -27,12 +37,77 @@ open class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var w:Window = window
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         initBanner()
+        initCategory()
+        initPopular()
+    }
+
+    private fun initPopular() {
+        val myref: DatabaseReference = database.getReference("Items")
+        binding.progressBarPopular.visibility = View.VISIBLE
+        val items:ArrayList<itemsDomain> = ArrayList()
+        myref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for (issue in snapshot.children) {
+                        val itemsDomain = issue.getValue(itemsDomain::class.java)
+                        if (itemsDomain != null) {
+                            items.add(itemsDomain)
+                        }
+                    }
+                    if(!items.isEmpty()){
+                        binding.recyclerViewPopular.layoutManager = LinearLayoutManager(this@Home, LinearLayoutManager.HORIZONTAL, false)
+                        binding.recyclerViewPopular.adapter = PopularAdapter(items)
+                    }
+                    binding.progressBarPopular.visibility = View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun initCategory() {
+        val myref: DatabaseReference = database.getReference("Category")
+        binding.progressBarCategory.visibility = View.VISIBLE
+        val items:ArrayList<CategoryDomain> = ArrayList()
+        myref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for (issue in snapshot.children) {
+                        val categoryDomain = issue.getValue(CategoryDomain::class.java)
+                        if (categoryDomain != null) {
+                            items.add(categoryDomain)
+                        }
+                    }
+                    if(!items.isEmpty()){
+                        binding.recyclerViewCategory.layoutManager = LinearLayoutManager(this@Home, LinearLayoutManager.HORIZONTAL, false)
+                        binding.recyclerViewCategory.adapter = CategoryAdapter(items)
+                    }
+                    binding.progressBarCategory.visibility = View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
     }
 
     private fun initBanner() {
