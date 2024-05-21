70e814fd20868c5f580f042e2cc6efc0dc979668
DANIEL HAROLDO REYES GARCIA <dreyesg11@miumg.edu.gt>
2024-05-21 17:36:27 -0600

    Se actualiza SharedPreferences al iniciar sesion

--- app/src/main/java/com/example/ferreexpress/Activity/AuthActivity.kt
+++ app/src/main/java/com/example/ferreexpress/Activity/AuthActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ferreexpress.Activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -23,27 +24,28 @@ import com.google.firebase.database.FirebaseDatabase
 
 class AuthActivity : AppCompatActivity() {
 
-    var database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    var firebaseUser : FirebaseUser?=null
+    private lateinit var database: FirebaseDatabase
+    private var firebaseUser: FirebaseUser? = null
+
     companion object {
         private const val RC_SIGN_IN = 9001
     }
 
-    //private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContentView(R.layout.activity_auth)
+
+        // Initialize Firebase components
         database = FirebaseDatabase.getInstance()
 
-        //ANALYTICS EVENT
+        // Analytics event
         val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
-        //setup
+        // Setup UI elements and listeners
         setup()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainHome)) { v, insets ->
@@ -51,10 +53,9 @@ class AuthActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 
-    private fun setup(){
+    private fun setup() {
         title = "Autenticacion"
         val btnRegistro = findViewById<Button>(R.id.btnregistro)
         val btnIniciarSesion = findViewById<Button>(R.id.btnInicioSesion)
@@ -62,40 +63,41 @@ class AuthActivity : AppCompatActivity() {
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
 
-        btnRegistro.setOnClickListener{
-            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
-                FirebaseAuth.getInstance().
-                createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener(){
-                    if(it.isSuccessful){
-                        showRegistro()
-                    }else{
-                        showAlert()
+        btnRegistro.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            showRegistro()
+                        } else {
+                            showAlert()
+                        }
                     }
-                }
             }
         }
 
-        btnIniciarSesion.setOnClickListener(){
-            if(emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
-                FirebaseAuth.getInstance().
-                signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener(){
-                    if(it.isSuccessful){
-                        showRegistro()
-                    }else{
-                        showAlert()
+        btnIniciarSesion.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            fetchAndStoreUserData() // Llama a fetchAndStoreUserData cuando el inicio de sesión sea exitoso
+                        } else {
+                            showAlert()
+                        }
                     }
-                }
             }
         }
 
-        // Dentro del método setup()
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id)) // Reemplaza con tu ID de cliente web
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
         val googleApiClient = GoogleApiClient.Builder(this)
-            .enableAutoManage(this) { /* Manejar errores de conexión */ }
+            .enableAutoManage(this) { }
             .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
             .build()
 
@@ -105,7 +107,49 @@ class AuthActivity : AppCompatActivity() {
         }
     }
 
-    private fun showAlert(){
+    private fun fetchAndStoreUserData() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        if (userId != null) {
+            val userRef = database.reference.child("Users").child(userId)
+            userRef.get().addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    val userData = dataSnapshot.value as Map<String, String>
+                    val sharedPref = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+                    with(sharedPref.edit()) {
+                        putString("name", userData["name"])
+                        putString("phone", userData["phone"])
+                        putString("depa", userData["depa"])
+                        putString("muni", userData["muni"])
+                        putString("barrio", userData["barrio"])
+                        putString("calle", userData["calle"])
+                        putString("address", userData["address"])
+                        putString("usuario", userId)
+                        putString("type", userData["type"])
+                        putString("profileImageUrl", userData["profileImageUrl"])
+                        apply()
+                    }
+                    Toast.makeText(this, "Bienvenido ${userData["name"]}", Toast.LENGTH_SHORT).show()
+                    showHome() // Llama a showHome después de actualizar los datos
+                } else {
+                    Toast.makeText(this, "Error al obtener datos del usuario", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "Error al conectar con la base de datos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showHome() {
+        val homeIntent = Intent(this, MainActivity::class.java)
+        startActivity(homeIntent)
+    }
+
+    private fun showRegistro() {
+        val registroIntent = Intent(this, RegistroUserActivity::class.java)
+        startActivity(registroIntent)
+    }
+
+    private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage("Ha ocurrido un error en la autenticacion del usuario")
@@ -114,40 +158,26 @@ class AuthActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun showHome(){
-        val homeIntent = Intent(this, MainActivity::class.java).apply {
-        }
-        startActivity(homeIntent)
-    }
-
-    private fun showRegistro(){
-        val registroIntent = Intent(this, RegistroUserActivity::class.java).apply {
-        }
-        startActivity(registroIntent)
-    }
-
-
-    private fun ComprobarSesion(){
+    private fun comprobarSesion() {
         firebaseUser = FirebaseAuth.getInstance().currentUser
-        if (firebaseUser!=null){
+        if (firebaseUser != null) {
             val intent = Intent(this@AuthActivity, MainActivity::class.java)
-            Toast.makeText(applicationContext, "La sesión esta Activa", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "La sesión está activa", Toast.LENGTH_SHORT).show()
             startActivity(intent)
             finish()
         }
     }
 
     override fun onStart() {
-        ComprobarSesion()
         super.onStart()
+        comprobarSesion()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_SIGN_IN && data != null) {
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if (result != null && result.isSuccess) {
+            if (result?.isSuccess == true) {
                 val account = result.signInAccount
                 if (account != null) {
                     firebaseAuthWithGoogle(account)
@@ -157,6 +187,8 @@ class AuthActivity : AppCompatActivity() {
             } else {
                 showAlert()
             }
+        } else {
+            showAlert() // Handle the case when data is null
         }
     }
 
@@ -165,13 +197,10 @@ class AuthActivity : AppCompatActivity() {
         FirebaseAuth.getInstance().signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success
-                    showRegistro()
+                    fetchAndStoreUserData()
                 } else {
-                    // Sign in failed
                     showAlert()
                 }
             }
     }
-
 }
